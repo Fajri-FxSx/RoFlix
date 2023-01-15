59cdc8ae2777c13690a20e799f5cb7c66bb4b402
Fajri-FxSx <108572333+Fajri-FxSx@users.noreply.github.com>
2023-01-15 18:50:45 +0700

    Delete apiConfig.js
    Signed-off-by: Fajri-FxSx <108572333+Fajri-FxSx@users.noreply.github.com>

--- src/api/apiConfig.js
+++
@@ -1,9 +0,0 @@
-const apiConfig = {
-  baseUrl: 'https://api.themoviedb.org/3/',
-  apiKey: '24661393e867b3b4a85f3f759a76a9d5',
-  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500${imgPath}`,
-  w200Image: (imgPath) => `https://image.tmdb.org/t/p/w200${imgPath}`,
-};
-
-export default apiConfig;
